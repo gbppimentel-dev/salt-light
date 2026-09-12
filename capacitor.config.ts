69121aa1b489ae2145f31502ce0815254c2b0deb
Glenn Pimentel <gbppimentel@icloud.com>
2026-09-12 23:36:19 +0800

    capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.glennpimentel.saltandlight',
+  appName: 'Salt & Light',
+  webDir: 'dist'
+};
+
+export default config;
